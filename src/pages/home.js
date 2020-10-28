73067a11a4b0bf3d068bc0f3aea0e7d8c62c8c85
isaac-adedoyin <aderonmuisaac@gmail.com>
2020-10-28 04:56:13 +0100

    feat: add header to home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { HeaderContainer } from '../containers/header';
 import { FaqsContainer } from '../containers/faqs';
 import { FooterContainer } from '../containers/footer';
 import { JumbotronContainer } from '../containers/jumbotron';
 
+
 export default function Home() {
     return (
         <>
+            <HeaderContainer />
             <JumbotronContainer />
             <FaqsContainer />
             <FooterContainer />
